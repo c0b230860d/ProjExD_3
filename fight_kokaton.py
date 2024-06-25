fa29411eac6034f9a257f4be9477a9d513ed1aef
c0b23086 <c0b230860d@edu.teu.ac.jp>
2024-06-25 17:35:26 +0900

    追加機能4:途中まで(デバック中)

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -3,6 +3,7 @@ import random
 import sys
 import time
 import pygame as pg
+import math
 
 
 WIDTH = 1100  # ゲームウィンドウの幅
@@ -56,6 +57,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)] #クラス(birdと同じ)
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -82,6 +84,8 @@ class Bird:
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
+        if sum_mv != [0, 0]:
+            self.dire = sum_mv
         screen.blit(self.img, self.rct)
 
  
@@ -98,7 +102,11 @@ class Beam:
         self.rct = self.img.get_rect()
         self.rct.centery = bird.rct.centery
         self.rct.left = bird.rct.right
-        self.vx, self.vy = +5, 0
+        self.vx, self.vy = bird.dire
+        self.kakudo = math.atan2(-self.vy, self.vx)
+        self.img = pg.transform(self.img, math.degrees(self.kakudo), 1.0)
+        self.rct.width = self.rct.centerx + (bird.rct.right - bird.rct.left) * (self.vx)/5
+        self.rct.height =  self.rct.centery + (bird.rct.bottom - bird.rct.top) * (self.vy)/5
 
     def update(self, screen: pg.Surface):
         """
